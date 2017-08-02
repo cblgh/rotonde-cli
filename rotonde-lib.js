291f413302e8f71afa6fb8fcad7ed519db168e24
cblgh <cblgh@cblgh.org>
2017-08-02 18:07:43 +0200

    refactor follow & unfollow

--- rotonde-lib.js
+++ rotonde-lib.js
@@ -17,30 +17,63 @@ function save(rotondePath) {
 
 function write(text, url, media, focus) {
     util.data().then((rotondeItems) => {
-        var [rotonde, settings] = rotondeItems;
-        var hash = crypto.createHash("sha256").update(text).digest("hex");
+        var [rotonde, settings] = rotondeItems
+        var hash = crypto.createHash("sha256").update(text).digest("hex")
         var entry = {"text": text, "time": parseInt((new Date).getTime() / 1000), "id": hash}
 
-        if (url) { entry["url"] = url; }
-        if (media) { entry["media"] = media; }
-        if (focus) { entry["focus"] = focus; }
+        if (url) { entry["url"] = url }
+        if (media) { entry["media"] = media }
+        if (focus) { entry["focus"] = focus }
 
-        rotonde["feed"].push(entry);
+        rotonde["feed"].push(entry)
         fs.writeFile(settings["rotonde location"], JSON.stringify(rotonde), function(err) {
             if (err) { 
-                console.error("err: couldn't save to", settings["rotonde location"]);
-                console.error(err);
-                return;
+                console.error("err: couldn't save to", settings["rotonde location"])
+                console.error(err)
+                return
             }
-            console.log("the entry was published", text);
+            console.log("the entry was published", text)
         })
     })
 }
 
 function follow(portal) {
+    util.data().then((rotondeItems) => {
+        var [rotonde, settings] = rotondeItems
+        if (rotonde["portal"].indexOf(portal) >= 0) {
+            console.log("already following", portal)
+            return
+        }
+        rotonde["portal"].push(portal) // save the portal we want to follow
+        fs.writeFile(settings["rotonde location"], JSON.stringify(rotonde), function(err) {
+            if (err) { 
+                console.error("err: couldn't save portal to rotonde.json")
+                console.error(err)
+                return
+            }
+            console.log("now following", portal)
+        })
+    })
 }
 
 function unfollow(portal) {
+    util.data().then((rotondeItems) => {
+        var [rotonde, settings] = rotondeItems
+        var index = rotonde["portal"].indexOf(portal)
+        if (index < 0) {
+            console.log("err: you aren't following", portal)
+            return
+        }
+        rotonde["portal"].splice(rotonde["portal"].indexOf(portal), 1) // remove portal we want to unfollow
+        fs.writeFile(settings["rotonde location"], JSON.stringify(rotonde), function(err) {
+            if (err) { 
+                console.error("err: couldn't save to", settings["rotonde location"])
+                console.error(err)
+                return
+            }
+            console.log("no longer following", portal)
+        })
+    })
 }
 
 function color(c) {
