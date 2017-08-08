9bfbd5b9223d1a88703b1d479edb98b95c401108
Alexander Cobleigh <cblgh@cblgh.org>
2017-08-08 18:43:13 +0200

    add saveSettings method to rotonde-utils

--- rotonde-utils.js
+++ rotonde-utils.js
@@ -1,29 +1,39 @@
-var fs = require("fs");
-var osenv = require("osenv");
-var path = require("path");
+var fs = require("fs")
+var osenv = require("osenv")
+var path = require("path")
 
 var rotondedir = path.resolve(osenv.home(), ".config", "rotonde")
 module.exports = {
     dir: rotondedir,
     saveFile: saveFile,
     settings: readSettings,
+    saveSettings: saveSettings,
     data: function() { 
-        return readSettings().then((settings) => {
-            return new Promise((resolve, reject) => {
-                fs.readFile(settings["rotonde location"], (err, data) => {
-                    if (err) { console.log("error reading rotonde location at ", settings["rotonde location"]); reject(err) };
-                    resolve([JSON.parse(data), settings]); // parse rotonde info & pass along settings
-                });
-            });
+        return readSettings().then(function (settings) {
+            return new Promise(function (resolve, reject) {
+                fs.readFile(settings["rotonde location"], function (err, data) {
+                    if (err) { console.log("error reading rotonde location at ", settings["rotonde location"]); reject(err) }
+                    resolve([JSON.parse(data), settings]) // parse rotonde info & pass along settings
+                })
+            })
         })
     }
 }
 
 function readSettings() {
-    return new Promise((resolve, reject) => {
+    return new Promise(function(resolve, reject) {
         fs.readFile(path.resolve(rotondedir, ".rotonde"), function(err, data) {
-            if (err) { reject(err); }
-            resolve(JSON.parse(data));
+            if (err) { reject(err) }
+            resolve(JSON.parse(data))
+        })
+    })
+}
+
+function saveSettings(settings) {
+    return new Promise(function (resolve, reject) {
+        fs.writeFile(path.resolve(rotondedir, ".rotonde"), JSON.stringify(settings), function(err, data) {
+            if (err) { reject(err) }
+            resolve()
         })
     })
 }
@@ -32,10 +42,10 @@ function readSettings() {
 function saveFile(settings, rotonde, successMsg) {
     fs.writeFile(settings["rotonde location"], JSON.stringify(rotonde), function(err) {
         if (err) { 
-            console.error("err: couldn't save to", settings["rotonde location"]);
-            console.error(err);
-            return;
+            console.error("err: couldn't save to", settings["rotonde location"])
+            console.error(err)
+            return
         }
-        console.log(successMsg);
+        console.log(successMsg)
     })
 }
